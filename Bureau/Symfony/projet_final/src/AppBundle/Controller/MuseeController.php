c184abc1e158dfd68676a1f55fa79dc80c1de09b
belkacemH <kasi_bej@yahoo.fr>
2017-03-19 20:05:40 +0100

    creation de entity Commentaire et une action d'un formulaire

--- Bureau/Symfony/projet_final/src/AppBundle/Controller/MuseeController.php
+++ Bureau/Symfony/projet_final/src/AppBundle/Controller/MuseeController.php
@@ -2,20 +2,24 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Commentaire;
 use AppBundle\Repository;
+use Doctrine\DBAL\Types\StringType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 
-class MuseeController extends Controller{
+class MuseeController extends Controller
+{
 
     /**
      * @Route("/musee/{page}",  name="musee")
      */
-    public function indexAction($page){
-        $nbParPage=10;
+    public function indexAction($page)
+    {
+        $nbParPage = 10;
 
         $listeMusee = $this->getDoctrine()
             ->getManager()
@@ -35,7 +39,6 @@ class MuseeController extends Controller{
     }
 
 
-
     /**
      * @Route("/info/{id}",  name="museeinfo")
      */
@@ -45,10 +48,56 @@ class MuseeController extends Controller{
             ->getDoctrine()
             ->getManager()
             ->getRepository('AppBundle:Musee');
-        $musee = $repository->findById(array('id'=> $id));
+        $musee = $repository->findById(array('id' => $id));
         return $this->render('musee/findOneMusee.html.twig', array(
             'infomusee' => $musee,));
     }
-}
 
+
+
+    /**
+     * @Route("/form/{request}",  name="form")
+     */
+
+    public function addAction(Request $request)
+
+    {
+
+        // On crée un objet Commentaire
+
+        $com = new Commentaire();
+
+
+        // On crée le FormBuilder grâce au service form factory
+
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $com);
+
+
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+
+        $formBuilder
+            ->add('auteur', TextType::class)
+            ->add('date', DateType::class)
+            ->add('note', StringType::class)
+            ->add('contenu', TextType::class);
+
+        // À partir du formBuilder, on génère le formulaire
+
+        $form = $formBuilder->getForm();
+
+
+        // On passe la méthode createView() du formulaire à la vue
+
+        // afin qu'elle puisse afficher le formulaire toute seule
+
+        return $this->render('musee/findOneMusee.html.twig', array(
+
+            'form' => $form,
+
+        ));
+
+    }
+
+
+}
 ?>
